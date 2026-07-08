869b44971ed4d45b995c83a47624a11c8320a4c6
nandinikunchala <kunchalanandini21@gmail.com>
2026-07-08 22:11:47 +0530

    variables

--- DAY-1/variables.py
+++ DAY-1/variables.py
@@ -5,6 +5,7 @@
 #To specify the data type of a variable, we can use casting.
 #variables are not dynamic.
 
+
 #RULES FOR PYTHON VARIABLES
 # A variable name must start with a letter or the underscore character
 # A variable name cannot start with a number
@@ -12,12 +13,14 @@
 # Variable names are case-sensitive (age, Age and AGE are three different variables)
 # Cannot use python keywords as variable names (e.g., if, else, while, for, etc.)
 
+
 #MULTI WORDS VARIABLES NAMES
 #Variable names with more than one word can be difficult to read.
 #There are several techniques you can use to make them more readable:
 #Camel Case: Each word, except the first, starts with a capital letter: myVariableName
 #Pascal Case: Each word starts with a capital letter: MyVariableName
 #Snake Case: Each word is separated by an underscore character: my_variable_name
+
 
 #UNPACK A COLLECTION
 #If we have a collection of values in a list, tuple etc.
@@ -27,3 +30,7 @@ x, y, z = fruits
 print(x)
 print(y)
 print(z)
+
+#GLOBAL VARIABLES
+#Variables that are created outside of a function are known as global variables.
+#To create a global variable inside a function, you can use the global keyword.
