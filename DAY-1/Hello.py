41b9c8211de4b3302c11e2a86764e72eb1daaba0
nandinikunchala <kunchalanandini21@gmail.com>
2026-07-08 17:45:16 +0530

    added

--- DAY-1/Hello.py
+++ DAY-1/Hello.py
@@ -1,19 +1,19 @@
 #print is used to display output to the console
-#print("Hello, World!")
+print("Hello, World!")
 #Indentation is important in Python, as it defines the structure of the code
-# if 10<100:
-#     print("10 is less than 100")
+if 10<100:
+    print("10 is less than 100")
 # Data types 
-# print(type("100"))#<class 'str'>
-# print(type(100))#<class 'int'>
-# print(type(10.0))#<class 'float'>
-# print(type(True))#<class 'bool'>        
-# print(type(None))#<class 'NoneType'>
-# print(type([1, 2, 3]))#<class 'list'>
-# print(type((1, 2, 3)))#<class 'tuple'>
-# print(type({1, 2, 3}))#<class 'set'>
-# print(type({"A":1}))#<class 'dict'>
-# a = True
-# print(type(a))#<class 'bool'> not str because no quotes are used
-# a = {}
-# print(type(a))
+print(type("100"))#<class 'str'>
+print(type(100))#<class 'int'>
+print(type(10.0))#<class 'float'>
+print(type(True))#<class 'bool'>        
+print(type(None))#<class 'NoneType'>
+print(type([1, 2, 3]))#<class 'list'>
+print(type((1, 2, 3)))#<class 'tuple'>
+print(type({1, 2, 3}))#<class 'set'>
+print(type({"A":1}))#<class 'dict'>
+a = True
+print(type(a))#<class 'bool'> not str because no quotes are used
+a = {}
+print(type(a))#<class 'dict'> not set because no values are used
