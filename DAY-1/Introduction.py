dab4f7617d31154e713a0662a03b36e434878a97
nandinikunchala <kunchalanandini21@gmail.com>
2026-07-08 21:45:43 +0530

    Python Intro

--- DAY-1/Introduction.py
+++ DAY-1/Introduction.py
@@ -1,17 +1,23 @@
 #Python
 # Python is a high-level, interpreted programming language known for its simplicity and readability.
 # Python is a popular programming language. It was created by Guido van rossum and released in 1991.
+
+
 # #It is used for
 # 1. Web Development (e.g., Django, Flask)
 # 2.Software Development
 # 3.Mathematics
 # 4. System Scripting
+
+
 # #What Python can do?
 # 1. Python can be used on a server to create web applications.
 # 2. Python can be used alongside software to create workflows.
 # 3. Python can connect to database systems. It can also read and modify files.
 # 4. Python can be used to handle big data and perform complex mathematics.
 # 5. Python can be used for rapid prototyping.
+
+
 # #Why Python?
 # 1. Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
 # 2. Python has a simple syntax similar to the English language.
@@ -19,9 +25,31 @@
 # 1.It was designed to be easy to read and write, making it accessible for beginners.
 # 2.Python uses new lines to complete a command while the other programming languages use semicolons or parentheses.
 # 3.Python relies on indentation,using white space to define the scope in code.
+
+
 # #Python syntax
 # Syntax is the set of rules for writing code correctly.
-print("Hello, World!") #This is a simple Python program that prints "Hello, World!" to the console.
+# print("Hello, World!") #if parantheses is also missing, it will give an error.
+
+
+# #Indentation
+# Indentation is very important in Python. It is used to define the blocks of code. It refers to whitespaces at 
+# the beginning of a code line. Max 4 atleast 1 space is used for indentation.
+
+
+# #Statements
+# In programming language ,the programming instructions are called statements.
+# In python , a statement usually ends when the line ends.we don't use semicolons like other programming languages.
+
+
+# #Print text
+# print() function is used to display output values.
+
+
+# #Print without new line
+# If we want to print multiple words on same line, we can use the end parameter.
+# end=""
+
 
 
  
