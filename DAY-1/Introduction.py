d4da649240863551d97b36edec3c574517caa7f6
nandinikunchala <kunchalanandini21@gmail.com>
2026-07-08 21:43:59 +0530

    Python Introduction

---
+++ DAY-1/Introduction.py
@@ -0,0 +1,27 @@
+#Python
+# Python is a high-level, interpreted programming language known for its simplicity and readability.
+# Python is a popular programming language. It was created by Guido van rossum and released in 1991.
+# #It is used for
+# 1. Web Development (e.g., Django, Flask)
+# 2.Software Development
+# 3.Mathematics
+# 4. System Scripting
+# #What Python can do?
+# 1. Python can be used on a server to create web applications.
+# 2. Python can be used alongside software to create workflows.
+# 3. Python can connect to database systems. It can also read and modify files.
+# 4. Python can be used to handle big data and perform complex mathematics.
+# 5. Python can be used for rapid prototyping.
+# #Why Python?
+# 1. Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+# 2. Python has a simple syntax similar to the English language.
+# #Python syntax compared to other programming languages
+# 1.It was designed to be easy to read and write, making it accessible for beginners.
+# 2.Python uses new lines to complete a command while the other programming languages use semicolons or parentheses.
+# 3.Python relies on indentation,using white space to define the scope in code.
+# #Python syntax
+# Syntax is the set of rules for writing code correctly.
+print("Hello, World!") #This is a simple Python program that prints "Hello, World!" to the console.
+
+
+ 
