3957315b6eba48a5743ceecfef042af9e4e0071a
nandinikunchala <kunchalanandini21@gmail.com>
2026-07-09 20:18:24 +0530

    Escape characters

---
+++ DAY-1/DAY-3/DAY-4/Escape_characters.py
@@ -0,0 +1,15 @@
+#1.Print:He said, "Python is easy."
+print('He said, "Python is easy."')
+
+#2.Print:C:\Users\Student\Desktop
+print("C:\\Users\\Student\\Desktop")
+
+#3.Print three words on three different lines using escape characters.
+print("Nandini\nLove\nDance")
+
+#4.Print data in tabular format using \t.
+print("I\tAM\tNANDINI")
+print("I\tLOVE\tDANCE")
+
+#5.Print both single and double quotes in the same sentence using escape characters.
+print("I am \"Nandini\" and I am \'20\' years old")
