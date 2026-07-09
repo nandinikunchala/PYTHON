530939484a657a41186dd3f32d5d38bcf21d80c0
nandinikunchala <kunchalanandini21@gmail.com>
2026-07-09 19:48:33 +0530

    string concatenation problems

---
+++ DAY-1/DAY-3/DAY-4/Strings_concatenation_problems.py
@@ -0,0 +1,27 @@
+#1.Concatenate two strings without adding a space.
+a="Hello"
+b="Nandini"
+print(a+b)
+
+#2.Concatenate two strings with a space.
+a="Hello"
+b="Nandini"
+print(a+" "+b)
+
+#Concatenate three strings separated by hyphens (-).
+a = "Python"
+b = "Java"
+c = "C"
+result = "-".join([a, b, c])
+print(result)
+
+#4.Repeat a string 5 times.
+a = "Hi"
+result = a * 5
+print(result)
+
+#5.Create a username by joining first name and last name with an underscore (_).
+first_name = "Nandini"
+last_name = "Kunchala"
+username = first_name + "_" + last_name
+print(username)
