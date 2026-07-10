6607c189e7b6d45ac16898b7ba5eaa1bb913d252
nandinikunchala <kunchalanandini21@gmail.com>
2026-07-10 15:37:29 +0530

    operators

---
+++ DAY-1/DAY-5/Python_operators.py
@@ -0,0 +1,31 @@
+#Python Operators
+#operators are used to perform operations on variables and values
+
+#Arithmetic operators:
+#arithmetic operators are used with numeric values to perform common mathematical operations
+x = 15
+y = 4
+print(x + y)#Addition
+print(x - y)#Subtraction
+print(x * y)#Multiplication
+print(x / y)#Division
+print(x % y)#Modulus
+print(x ** y)#Exponentiation
+print(x // y)#Floor division
+
+#Assignment operators
+# =	    x = 5    x = 5	
+# +=	x += 3	 x = x + 3	
+# -=	x -= 3	 x = x - 3	
+# *=	x *= 3	 x = x * 3	
+# /=	x /= 3	 x = x / 3	
+# %=	x %= 3	 x = x % 3	
+# //=	x //= 3	 x = x // 3	
+# **=	x **= 3	 x = x ** 3	
+# &=	x &= 3	 x = x & 3	
+# |=	x |= 3	 x = x | 3	
+# ^=	x ^= 3	 x = x ^ 3	
+# >>=	x >>= 3	 x = x >> 3	
+# <<=	x <<= 3	 x = x << 3	
+# :=	print(x := 3) #walrus operator
+
