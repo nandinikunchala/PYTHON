a837c3acd4f71b731594aaa0992e407990475d0c
nandinikunchala <kunchala.nandini21@gmail.com>
2026-07-17 23:02:43 +0530

    strings

--- DAY-1/DAY-6/DAY-7/DAY-8/DAY-9/DAY-10/DAY-11/DAY-12/Strings_problems.py
+++ DAY-1/DAY-6/DAY-7/DAY-8/DAY-9/DAY-10/DAY-11/DAY-12/Strings_problems.py
@@ -123,3 +123,125 @@ for word in words:
     if len(word)<len(shortest):
         shortest=word
 print(shortest)
+
+
+#MEDIUM PROBLEMS
+# Count the frequency of every character.
+b="Manasa"
+target="a"
+print(b.count(target))
+# Count the frequency of every word.
+c="Satwika is rupa friend"
+words=c.split()
+frequency= {}
+for word in words:
+    if word in frequency:
+        frequency[word]+=1
+    else:
+        frequency[word]=1
+print(frequency)
+# Remove duplicate characters.
+d="Nandini"
+result=""
+for char in d.lower():
+    if char not in result:
+        result+=char
+print(result)
+# Print duplicate characters only.
+e = "Nandini"
+result = ""
+for char in e.lower():
+    if char in e.lower() and e.lower().count(char) > 1:
+        if char not in result:
+            result += char
+print(result)
+# Find the first non-repeating character.
+f="Madhav"
+count=0
+for char in f:
+    if f.count(char)==1:
+        print(char)
+        count+=1
+        if count==2:
+            break
+# Find the first repeating character.
+g="Madhav"
+for char in g:
+   if g.count(char)>1:
+       print(char)
+       break
+# Check whether two strings are anagrams.
+h="heart"
+i="earth"
+if sorted(h)==sorted(i):
+    print("anagrams")
+else:
+    print("Not anagrams")
+# Check whether two strings are rotations of each other.
+j="ABCD"
+k="CDAB"
+if len(j)==len(k) and k in (j+j):
+    print("Rotation")
+else:
+    print("No rotation")
+# Remove all vowels.
+l="Nandu"
+result="" 
+for char in l:
+    if char.lower() not in "aeiou":
+        result+=char
+print(result)
+# Remove all special characters.
+m="Nandy@!2185"
+result=""
+for char in m:
+    if char.isalnum():
+        result+=char
+print(result)
+# Count the number of words.
+n="Nandy"
+count=0
+for char in n:
+    count+=1
+print(count)
+# Reverse each word in a sentence.
+o="Nandini and Shivani are Friends"
+words=o.split()
+result=""
+for word in words:
+   rev=""
+   for char in word:
+     rev=char+rev
+
+   result+=rev+" "
+print(result)
+#Reverse the order of words.
+p="Veda and Nandini are friends"
+words=p.split()
+for i in range(len(words)-1,-1,-1):
+    print(words[i],end=" ")
+print()
+# Capitalize the first letter of every word without using .title().
+q="nandu is also nandini"
+words=q.split()
+result=""
+for word in words:
+    new_word=word[0].upper()+word[1:]
+    result+=new_word +" "
+print(result)
+# Find the most frequent character.
+# Find the least frequent character.
+# Compress a string.
+# Example:
+# aaabbccccd
+# Output:
+# a3b2c4d1
+# Expand a compressed string.
+
+# Example:
+
+# a3b2c4
+# Output:
+# aaabbcccc
+# Check whether one string is a subsequence of another.
+# Check whether all characters are unique.
