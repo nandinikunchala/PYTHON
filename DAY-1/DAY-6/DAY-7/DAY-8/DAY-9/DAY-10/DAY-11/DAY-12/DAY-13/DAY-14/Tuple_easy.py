e48032b3644f24f51d940f1b2823e78c4abc1ac1
nandinikunchala <kunchala.nandini21@gmail.com>
2026-07-20 22:19:12 +0530

    tuple

---
+++ DAY-1/DAY-6/DAY-7/DAY-8/DAY-9/DAY-10/DAY-11/DAY-12/DAY-13/DAY-14/Tuple_easy.py
@@ -0,0 +1,10 @@
+#Create a tuple with 5 integers and print it.
+# Print the first and last element of a tuple.
+# Find the length of a tuple without using len().
+# Check whether a given element exists in a tuple.
+# Count how many times an element appears in a tuple.
+# Find the index of an element in a tuple.
+# Concatenate two tuples.
+# Repeat a tuple 3 times.
+# Slice a tuple to print the first 3 elements.
+# Reverse a tuple using slicing.
