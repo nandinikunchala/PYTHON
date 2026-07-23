cef54ca230971b29025a1beb7d2c7badd3062d76
nandinikunchala <kunchala.nandini21@gmail.com>
2026-07-23 22:39:39 +0530

    practise

---
+++ DAY-1/DAY-6/DAY-7/DAY-8/DAY-9/DAY-10/DAY-11/DAY-12/DAY-13/DAY-14/DAY-15/DAY-17/Problems.py
@@ -0,0 +1,208 @@
+#Write a Python program to print
+print("Hello World")
+#Write a Python program to print the following exactly:
+# Name: Nandini
+# Age: 20
+# Course: Python
+print("Name"," Nandini",sep=":")
+print("Age","20",sep=":")
+print("Course","Python",sep=":")
+#Write a Python program to print this output using only ONE print() statement:
+# Python is easy
+# Python is powerful
+# Python is popular
+print("Python is easy","Python is powerful","Python is popular",end="\n")
+#Write a Python program to print:
+# Python-HTML-CSS-JavaScript
+# Requirements:
+# Use only ONE print() statement
+# Use the sep parameter
+print("Python","HTML","CSS","Javascript",sep="-")
+#Write a Python program to produce this exact output:
+# Hello World!
+# Requirements:
+# Use two print() statements
+# Use the end parameter
+# Do not use +
+print("Hello",end="")
+print(" World!")
+#Write a Python program to produce this exact output:
+# A-B-C-D
+# Requirements:
+# Use only ONE print() statement
+# Use the sep parameter
+# Don't type "A-B-C-D" as one complete string.
+print("A","B","C","D",sep="-")
+#Write a Python program to produce this exact output:
+# Name: Nandini
+# Age: 20
+# City: Hyderabad
+# Requirements:
+# Use only ONE print() statement
+# Use both sep and end parameters
+# Don't write the complete output as one string.
+print("Name"," Nandini\nAge"," 20\nCity"," Hyderabad",sep=":",end="\n")
+#Write a program to print:
+# 10
+# 20
+# 30
+# Requirements:
+# Use only ONE print() statement
+# Use the sep parameter
+print("10","20","30",sep="\n")
+#Write a Python program to produce this exact output:
+# Python
+# is
+# easy
+# Requirements:
+# Use only ONE print() statement
+# Use the sep parameter
+# Don't write the complete output as one string.
+print("Python","is","easy",sep="\n")
+#Write a Python program to produce exactly:
+# A1
+# B2
+# C3
+# Requirements:
+# Use only ONE print() statement
+# Use both sep and end
+# Don't write "A1\nB2\nC3" as one complete string.
+print("A1","B2","C3",sep="\n",end="\n")
+#Write a Python program to produce exactly:
+# Hello-World!
+# Requirements:
+# Use one print() statement
+# Use both sep and end
+# Do not write "Hello-World!" as one complete string
+print("Hello","World!",sep="-",end="\n")
+#Write a Python program to produce exactly:
+# PythonPythonPython
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Do not type "PythonPythonPython" directly.
+print("Python"*3)
+#Write a Python program to produce exactly:
+# AAAAABBB
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Use string concatenation (+)
+# Do not type "AAAAABBB" directly.
+print("A"*5+"B"*3)
+#Write a Python program to produce exactly:
+# Hi Hi Hi
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Use string concatenation (+)
+# Do not type "Hi Hi Hi" directly.
+print("Hi "*2+"Hi")
+#Write a Python program to produce exactly:
+# HelloHello WorldWorld
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Use string concatenation (+)
+# Do not type the complete output directly.
+print("Hello"*2+" "+"World"*2)
+#Write a Python program to produce exactly:
+# Python is easy
+# Requirements:
+# Use only ONE print() statement
+# Use string concatenation (+)
+# Do not type "Python is easy" as one complete string.
+print("Python"+" is"+" easy")
+#Write a Python program to produce exactly:
+# My name is Nandini
+# Requirements:
+# Use only ONE print() statement
+# Use string concatenation (+)
+# Use at least 4 separate strings
+# Do not type "My name is Nandini" as one complete string.
+print("My"+" name"+" is"+" Nandini")
+#Write a Python program to produce exactly:
+# 10 + 20 = 30
+# Requirements:
+# Use only ONE print() statement
+# Use string concatenation (+)
+# Use at least 5 separate strings
+# Do not type the complete output as one string.
+print("10 +"+" 20"+" "+"= "+"30")
+#Write a Python program to produce exactly:
+# I am 20 years old
+# Requirements:
+# Use only ONE print() statement
+# Use string concatenation (+)
+# Use at least 5 separate strings
+# Do not type the complete output as one string.
+print("I"+" am"+" 20"+" years"+" old")
+#Write a Python program to produce exactly:
+# **********
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Do not type 10 * characters directly.
+print("*"*10)
+#Write a Python program to produce exactly:
+# #####-----
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Use string concatenation (+)
+# Do not type all # and - characters directly.
+print("#"*5+"-"*5)
+#Write a Python program to produce exactly:
+# Hello
+# Hello
+# Hello
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Use the newline character \n
+# Do not type "Hello" three separate times.
+print("Hello\n"*3)
+#Write a Python program to produce exactly:
+# A-B-C
+# D-E-F
+# Requirements:
+# Use only ONE print() statement
+# Use sep
+# Use \n
+# Do not write the complete output as one string.
+print("A","B","C\nD","E","F",sep="-")
+#Write a Python program to produce exactly:
+# Python
+# PythonPython
+# PythonPythonPython
+# Requirements:
+# Use only ONE print() statement
+# Use string repetition (*)
+# Use \n
+# Do not type Python three separate times
+print("Python"+"\n"+"Python"*2+"\n"+"Python"*3)
+#Write a Python program to produce exactly:
+# 10
+# 20
+# 30
+# 40
+# Requirements:
+# Use only ONE print() statement
+# Use sep
+# Use \n
+# Do not type "10\n20\n30\n40" as one complete string.
+print("10","20","30","40",sep="\n")
+#Write a Python program to produce exactly:
+# Name: Nandini
+# Age: 20
+# Requirements:
+# Use only ONE print() statement
+# Use string concatenation (+)
+# Use \n
+# Do not type the complete output as one string.
+print("Name:"+" Nandini"+"\n"+"Age:"+" 20")
+# #What will be the output of this code?
+# print("10" + "20")#1020(str)
+# print(10 + 20)#30(int)
+# Requirements:
+# Don't just give the output. Explain why the two outputs are different.
