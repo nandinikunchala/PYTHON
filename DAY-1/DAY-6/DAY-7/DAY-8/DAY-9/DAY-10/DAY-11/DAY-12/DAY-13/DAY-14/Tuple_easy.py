e9e6528ee369da8afb7bed157210ef395f7a6b89
nandinikunchala <kunchala.nandini21@gmail.com>
2026-07-23 22:44:49 +0530

    added

--- DAY-1/DAY-6/DAY-7/DAY-8/DAY-9/DAY-10/DAY-11/DAY-12/DAY-13/DAY-14/Tuple_easy.py
+++ DAY-1/DAY-6/DAY-7/DAY-8/DAY-9/DAY-10/DAY-11/DAY-12/DAY-13/DAY-14/Tuple_easy.py
@@ -1,7 +1,15 @@
 #Create a tuple with 5 integers and print it.
+x=(1,2,6,7,9)
+print(x)
 # Print the first and last element of a tuple.
+print(x[0],x[-1])
 # Find the length of a tuple without using len().
+count=0
+for num in x:
+    count+=1
+print(count)
 # Check whether a given element exists in a tuple.
+
 # Count how many times an element appears in a tuple.
 # Find the index of an element in a tuple.
 # Concatenate two tuples.
