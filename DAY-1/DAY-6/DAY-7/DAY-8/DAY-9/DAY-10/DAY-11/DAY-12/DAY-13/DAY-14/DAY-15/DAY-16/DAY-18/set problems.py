fa8f16d1b8994e2575511b4493227f45dcd70dbd
nandinikunchala <kunchala.nandini21@gmail.com>
2026-07-25 22:51:53 +0530

    added

---
+++ DAY-1/DAY-6/DAY-7/DAY-8/DAY-9/DAY-10/DAY-11/DAY-12/DAY-13/DAY-14/DAY-15/DAY-16/DAY-18/set problems.py
@@ -0,0 +1,20 @@
+#Create a tuple with 5 numbers and print it.
+t = (10, 20, 30, 40, 50)
+print(t)
+# Find the length of a tuple without using len().
+t = (10, 20, 30, 40, 50)
+count = 0
+for x in t:
+    count += 1
+print(count)
+# Print the first element of a tuple.
+t = (10, 20, 30, 40, 50)
+print(t[0])
+# Print the last element of a tuple.
+t = (10, 20, 30, 40, 50)
+print(t[-1])
+# Print all elements of a tuple using a for loop.
+t = (10, 20, 30, 40, 50)
+
+for x in t:
+    print(x)
