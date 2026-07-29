69401fc863d9831f51e96c2d0f6b65870d9bb29d
nandinikunchala <kunchala.nandini21@gmail.com>
2026-07-29 22:43:32 +0530

    added

--- DAY-15/Tuple_medium.py
+++ DAY-15/Tuple_medium.py
@@ -32,7 +32,18 @@ for x in a:
 print(s_largest)
 # Find the second smallest element.
 # Count even and odd numbers in a tuple.
+a=(2,3,4,5,6,7,8)
+even=0
+odd=0
+for x in a:
+    if x%2==0:
+        even+=1
+    else:
+        odd+=1
+print("Even numbers:",even)
+print("Odd numbers:",odd)
 # Create a new tuple containing only even numbers.
+c=(2,4,6,8,12,14)
 # Remove duplicate elements (create a new tuple).
 # Check whether a tuple is sorted in ascending order.
 # Merge two tuples and sort them.
