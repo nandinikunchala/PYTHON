2c1ab6d5b7eaa33ab69d025af491b949502fefbc
nandinikunchala <kunchala.nandini21@gmail.com>
2026-08-03 23:01:39 +0530

    added

---
+++ DAY-24/sets_prob.py
@@ -0,0 +1,15 @@
+#Create a set with 5 integers and print it.
+# Add an element to a set.
+# Add multiple elements to a set.
+# Remove an element from a set.
+# Remove an element using discard().
+# Remove and return a random element using pop().
+# Clear all elements from a set.
+# Find the number of elements in a set.
+# Check whether an element exists in a set.
+# Copy a set into another set.
+# Print all elements of a set using a loop.
+# Convert a list into a set.
+# Convert a tuple into a set.
+# Convert a string into a set of unique characters.
+# Find the maximum and minimum elements in a set.
