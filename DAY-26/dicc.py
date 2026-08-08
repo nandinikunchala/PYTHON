7a34c5a64a10e1fae7c8ed5d644bf419b2251405
nandinikunchala <kunchala.nandini21@gmail.com>
2026-08-08 21:55:02 +0530

    added

---
+++ DAY-26/dicc.py
@@ -0,0 +1,11 @@
+#dic problem
+s="hi nandini"
+count={}
+for char in s:
+    if char in count:
+        count[char]+=1
+    else:
+        count[char]=1
+for char in count:
+    if count[char]==3:
+        print({char:count[char]})
