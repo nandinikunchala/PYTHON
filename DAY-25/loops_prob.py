77ab1fb9af8f92ff2bf1174a840c62fac6195c40
nandinikunchala <kunchala.nandini21@gmail.com>
2026-08-10 22:51:43 +0530

    added

---
+++ DAY-25/loops_prob.py
@@ -0,0 +1,11 @@
+#Pattern
+# * * * * *
+for i in range(5):
+    print("*",end=" ")
+# #in 5 rows and 5 columns
+for i in range(5):
+    print("* "*5)
+#Print 5 stars in 5 rows
+for i in range(5):
+    print("* ")
+
