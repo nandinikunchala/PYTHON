95e10211d0ad24cbbb7682376a2b2b24a6cc528a
nandinikunchala <kunchala.nandini21@gmail.com>
2026-08-11 11:06:18 +0530

    added

--- DAY-25/loops_patt.py
+++ DAY-25/loops_patt.py
@@ -1,11 +1,13 @@
 #Pattern
 # * * * * *
 for i in range(5):
-    print("*",end=" ")
-# #in 5 rows and 5 columns
+     print("*",end=" ")
+# #in 5 columns
+for i in range(5):
+   print("* ")
+#print a square 5 rows and 5 columns
 for i in range(5):
     print("* "*5)
-#Print 5 stars in 5 rows
-for i in range(5):
-    print("* ")
-
+#
+for i in range(1,6):
+    print("* "*i)
