f0a190f595430436237a578b81058d731051db8c
nandinikunchala <kunchala.nandini21@gmail.com>
2026-08-11 15:25:07 +0530

    added

---
+++ DAY-31/scope.py
@@ -0,0 +1,34 @@
+#Scope
+#scope has 4 sub topics
+#L-local scope
+#E-enclosing scope
+#G-global scope
+#B-built in scope
+
+#local problems
+def num():
+    x=10
+    x=20
+    print(x)
+num()
+
+#global
+x=10
+def num():
+   global x
+   x+=3
+num()
+print(x)
+
+#enclosing problem
+x=10
+def outer():     #enclosing variable
+    x=20
+    def inner():
+        print(x) #finds x in enclosing scope
+    inner()
+outer()
+
+#built-in scope
+x="nandu"
+print(len(x))
