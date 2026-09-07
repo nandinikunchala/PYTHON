53efacfd638318af22e22b518530a39a2ab59f23
nandinikunchala <kunchala.nandini21@gmail.com>
2026-09-07 23:56:30 +0530

    practice problem

--- DAY-58/prac_prob.py
+++ DAY-58/prac_prob.py
@@ -1,1 +1,42 @@
-#
+#Write a python program to print company anme,product name,and service name on separate lines
+print("Company name:DELL")
+print("Product name:Laptop")
+print("Service name:Repair")
+#Write a python program that prints a formatted message using print() with multiple arguments.
+print("Hello","I'm Nandini")
+#Write a python program to print the following output exactly:Product:AI Resume Builder|Status:Active.
+print("Product:AI Resume Builder|Status:Active.")
+#Write a Python program that takes a user's name and prints a personalized welcome message.
+a=input("Enter a name:")
+print("Hi",a,"Welcome!")
+# Write a Python program to read a product name, price, and quantity from the user and display them.
+a=input("Enter product name:")
+b=input("Enter product price:")
+c=input("Enter quantity:")
+print(a,b,c)
+# Write a Python program that calculates the total price of a product using price and quantity entered by the user.
+a=input("Enter product name:")
+b=int(input("Enter product price:"))
+c=int(input("Enter quantity:"))
+total=b*c
+print(total)
+# Write a Python program to swap two variables without using a third variable.
+a=1
+b=2
+a,b=b,a
+print(a,b)
+# Write a Python program to create variables for customer_name, product_name, quantity, and price, then display an invoice line.
+q="Nandini"
+w="Laptop"
+e=1
+r=50000
+print(q,"purchased a",w,"of quantity",e,"price of",r)
+# Write a Python program that checks the type of each of these values: 100, 99.99, 'Python', True.
+print(type(100))
+print(type(99.99))
+print(type('Python'))
+print(type(True))
+# Write a Python program that stores a service name and service fee in variables and prints them in a readable format.
+k="Ac repair"
+l=10000
+print("Service name is",k,"and service price is",l)
