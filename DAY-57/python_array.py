f6c3253f3baeeb5200bdedbe7a3c8dd1f2027482
nandinikunchala <kunchala.nandini21@gmail.com>
2026-09-18 22:54:43 +0530

    array

---
+++ DAY-57/python_array.py
@@ -0,0 +1,20 @@
+#ARRAYS
+#Arrays are used to store multiple values in one single variable
+#An array is a special variable, which can hold more than one value at a time.
+
+#Access the Elements of an Array
+# You refer to an array element by referring to the index number.
+#x = cars[0]
+
+#The Length of an Array
+# Use the len() method to return the length of an array (the number of elements in an array).
+
+#Looping Array Elements
+# You can use the for in loop to loop through all the elements of an array.
+
+#Adding Array Elements
+# You can use the append() method to add an element to an array.
+
+#Removing Array Elements
+# You can use the pop() method to remove an element from the array.
+#You can also use the remove() method to remove an element from the array.
